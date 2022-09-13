b079a133128f22a50eb7b134e526363050ae9bec
Chethan M R <chethanj332@gmail.com>
2022-09-13 20:05:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+let waterLtr=document.querySelector('.remaining>h2');
+let percentage=document.querySelector('.percentage');
+let glass=document.querySelectorAll('.glass');
+let smallglass=document.querySelector('.small-glass');
+let bigBottle=document.querySelector('.remaining');
+
+
+
+bigBottleFill();
+
+glass.forEach((smallglass,i) => {
+    smallglass.addEventListener('click',()=>waterFill(i));
+});
+
+function waterFill(i){
+
+    if( glass[i].classList.contains('glass-full') && !glass[i].nextElementSibling.classList.contains('glass-full')){
+        i--;
+    }
+    
+    glass.forEach((smallglass,j) => {
+        if(j<=i){
+            smallglass.classList.add('glass-full');
+        }
+        else
+        smallglass.classList.remove('glass-full');
+    }
+    );
+
+    bigBottleFill();
+}
+
+function bigBottleFill(){
+    let glassFull=document.querySelectorAll('.glass-full').length;
+    let totalGlass=glass.length;
+    if(glassFull==0){
+        percentage.style.visibility='hidden';
+        percentage.style.height=0;
+    }
+    else{
+        percentage.style.visibility='visible';
+        percentage.style.height=`${(glassFull/totalGlass)*330 }px `;
+        percentage.innerText=  `${glassFull/totalGlass  *100}% `;
+    
+    }
+    if(glassFull==totalGlass){
+        bigBottle.style.visibility='hidden';
+        // bigBottle.style.height=0;
+    }
+    else{
+        bigBottle.style.visibility='visible';
+        waterLtr.innerText=`${2-(250*glassFull /1000)}L`
+    }
+
+}
+
+
+ 
+// for(let i=0;i<glass.length;i++){
+//     glass[i].addEventListener('click', function (){
+
+//         if( glass[i].classList.contains('glass-full') && !glass[i].nextElementSibling.classList.contains('glass-full')){
+//             glass[i]--;
+//         }
+
+//         for(let j=0;j<glass.length; j++){
+//             if(j<=i){
+//                 glass[j].classList.add('glass-full');
+//             }
+//             else 
+//             glass[j].classList.remove('glass-full');
+//     }
+            
+//         });
+//   }
+
+
+
